0fd00118991f473198aa5f37144a4eee8a39137c
5aVaGe <sergey340qwe@gmail.com>
2018-12-31 15:18:45 +0200

    init commit

---
+++ app/src/main/java/com/karavatskiy/serhii/babushkachat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.karavatskiy.serhii.babushkachat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
